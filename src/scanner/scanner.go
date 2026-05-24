5a85c0bdf58eedefcdd68e17c0195ee7ee27cf93
MQuang <65891056+quang2652001@users.noreply.github.com>
2026-05-24 10:57:41 +0700

    implement assignment and equality op

--- src/scanner/scanner.go
+++ src/scanner/scanner.go
@@ -9,50 +9,59 @@ import (
 )
 
 func Scan(content []byte) ([]token.Token, bool) {
-  content = bytes.TrimSpace(content)
+	content = bytes.TrimSpace(content)
 
 	tokens := []token.Token{}
-  hadError := false
+	hadError := false
 
 	for _, char := range content {
 		switch char {
 		case '(':
-      tokens = append(tokens, newToken(token.LEFT_PAREN, char, nil))
-    case ')':
-      tokens = append(tokens, newToken(token.RIGHT_PAREN, char, nil))
-    case '{':
-      tokens = append(tokens, newToken(token.LEFT_BRACE, char, nil))
-    case '}':
-      tokens = append(tokens, newToken(token.RIGHT_BRACE, char, nil))
-    case '.':
-      tokens = append(tokens, newToken(token.DOT, char, nil))
-    case ',':
-      tokens = append(tokens, newToken(token.COMMA, char, nil))
-    case '-':
-      tokens = append(tokens, newToken(token.MINUS, char, nil))
-    case '+':
-      tokens = append(tokens, newToken(token.PLUS, char, nil))
-    case ';':
-      tokens = append(tokens, newToken(token.SEMICOLON, char, nil))
-    case '*':
-      tokens = append(tokens, newToken(token.STAR, char, nil))
-    default:
-      printError(1, char)
-      hadError = true
-    }
+			tokens = append(tokens, newToken(token.LEFT_PAREN, string(char), nil))
+		case ')':
+			tokens = append(tokens, newToken(token.RIGHT_PAREN, string(char), nil))
+		case '{':
+			tokens = append(tokens, newToken(token.LEFT_BRACE, string(char), nil))
+		case '}':
+			tokens = append(tokens, newToken(token.RIGHT_BRACE, string(char), nil))
+		case '.':
+			tokens = append(tokens, newToken(token.DOT, string(char), nil))
+		case ',':
+			tokens = append(tokens, newToken(token.COMMA, string(char), nil))
+		case '-':
+			tokens = append(tokens, newToken(token.MINUS, string(char), nil))
+		case '+':
+			tokens = append(tokens, newToken(token.PLUS, string(char), nil))
+		case ';':
+			tokens = append(tokens, newToken(token.SEMICOLON, string(char), nil))
+		case '*':
+			tokens = append(tokens, newToken(token.STAR, string(char), nil))
+		case '=':
+			s := len(tokens)
+			if len(tokens) != 0 && tokens[s-1].TokenType == token.EQUAL {
+				tokens = tokens[:s-1]
+				tokens = append(tokens, newToken(token.EQUAL_EQUAL, "==", nil))
+			} else {
+				tokens = append(tokens, newToken(token.EQUAL, string(char), nil))
+			}
+		default:
+			printError(1, char)
+			hadError = true
+		}
 	}
 
-  tokens = append(tokens, newToken(token.EOF, byte(0), nil))
+	tokens = append(tokens, newToken(token.EOF, "", nil))
 
-  return tokens, hadError
+	return tokens, hadError
 }
 
-func newToken(tokenType token.TokenType, char byte, value interface{}) token.Token{
-  return token.Token{
-    TokenType: tokenType,
-    Text: string(char),
-    Value: value, }
+func newToken(tokenType token.TokenType, tokenStr string, value interface{}) token.Token {
+	return token.Token{
+		TokenType: tokenType,
+		Text:      tokenStr,
+		Value:     value}
 }
 
 func printError(line int, char byte) {
-  fmt.Fprintf(os.Stderr, "[line %d] Error: Unexpected character: %s\n", line, string(char)) }
+	fmt.Fprintf(os.Stderr, "[line %d] Error: Unexpected character: %s\n", line, string(char))
+}
